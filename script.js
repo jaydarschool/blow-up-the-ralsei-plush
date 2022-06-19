e889149321157048f646eb50945d750143ff4c96
Jaydar <99223349+jaydar10@users.noreply.github.com>
2022-06-19 14:36:04 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const explodeBtn = document.getElementById("explosion-button");
+var ralPlush = document.getElementById("ral-plush");
+
+
+const explosionGif = "https://c.tenor.com/-pMfQcryj3cAAAAi/explosion-boom.gif";
+
+function explosion() {
+  ralPlush.src = explosionGif;
+};
